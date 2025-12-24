8b9a8f068e1fd1fce5339856ac83608b55fc7fd2
Saurav Upadhyay <saurav@results4america.org>
2025-12-24 10:17:08 -0500

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Jurisdiction-data-pull/',
+  base: '/open-data-dashboard-map/',
   plugins: [react()],
 })
+
